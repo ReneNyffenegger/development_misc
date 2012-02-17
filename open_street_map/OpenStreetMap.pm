34226e3f40cae881d5e8d14e410536032c93a7cf
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-02-17 17:47:59 +0100

    + keyValueInMap

--- open_street_map/OpenStreetMap.pm
+++ open_street_map/OpenStreetMap.pm
@@ -2,6 +2,8 @@ package OpenStreetMap;
 
 require LWP::UserAgent;
 
+use XML::Parser;
+
 sub new {
   my $self = {};
 
@@ -42,5 +44,97 @@ sub map {
 
   return $self -> api_call_('map', "bbox=$west,$south,$east,$north");
 }
+
+sub keyValueInMap {
+
+#   This function takes a bounding box ($west .. $east, $north .. $south) 
+#   and retrieves the distinct keys, values, and ids.
+#   See test.pl for more details.
+
+  my $self = shift;
+
+  my $west   = shift;
+  my $east   = shift;
+  my $north  = shift;
+  my $south  = shift;
+
+  my $text = $self -> map($west, $east, $north, $south);
+
+  my $xml_parser = new XML::Parser(Style=>'Stream');
+
+  $xml_parser -> setHandlers (
+      Start   => \&xml_start_element,
+      End     => \&xml_end_element,
+      Char    => \&xml_character_data,
+      Default => \&xml_default,
+      Final   => \&xml_final # what xml_parser -> parse($text) will return.
+  );
+
+  return $xml_parser -> parse($text);
+}
+
+#   { Functions used by XML::Parser, see setHandlers above.
+
+sub xml_start_element {
+
+  my($expat, $element, %attributes) = @_;
+
+  if ($element eq 'relation' or $element eq 'way' or $element eq 'node') {
+      die if $expat -> {node_way_or_relation};
+      $expat -> {node_way_or_relation} = $element;
+  }
+
+  my $k;
+  my $v;
+
+  foreach my $attribute (keys %attributes) {
+   
+    if ($element eq $expat -> {node_way_or_relation} and $attribute eq 'id') {
+       $expat -> {current_id} = $attributes{$attribute};
+    }
+    elsif ($element eq 'tag') {
+
+      if ($attribute eq 'k') {
+        $k = $attributes{$attribute};
+      }
+      elsif ($attribute eq 'v') {
+        $v = $attributes{$attribute};
+      }
+
+    }
+  }
+
+  if ($element eq 'tag') {
+     push @{$expat->{returned} -> {$k} -> {$v} -> {$expat->{node_way_or_relation}}}, $expat->{current_id};
+  }
+}
+
+sub xml_end_element {
+  my($expat, $element, %attributes) = @_;
+
+  if ($expat -> {node_way_or_relation} and
+      $expat -> {node_way_or_relation} eq $element) {
+
+    $expat -> {node_way_or_relation} = "";
+  }
+}
+
+sub xml_character_data {
+  my($expat, $data) = @_;
+  $data =~ s/{amp}/&/;
+}
+
+sub xml_final {
+
+  my($expat) = @_;
+
+  return $expat->{returned};
+}
+
+# }
+
+sub xml_default {
+}
+
 
 1;
--- open_street_map/test.pl
+++ open_street_map/test.pl
@@ -7,4 +7,11 @@ my $osm = new OpenStreetMap;
 
 print $osm -> capabilities;
 
-print $osm -> map(8.538386, 8.544888, 47.369598, 47.365929);
+#print $osm -> map(8.538386, 8.544888, 47.369598, 47.365929);
+
+my $kv = $osm -> keyValueInMap(8.538, 8.539, 47.370, 47.369);
+
+foreach my $k       (keys %{$kv                  }) { print "$k\n";
+foreach my $v       (keys %{$kv->{$k}            }) { print "  $v\n";
+foreach my $nwr     (keys %{$kv->{$k}->{$v}      }) { print "     $nwr\n";
+foreach my $node_id (     @{$kv->{$k}->{$v}{$nwr}}) { print "         $node_id\n"; } } } }
