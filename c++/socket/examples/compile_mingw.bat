6ed90ee0e183d18576e94f891b637959b9ddc88c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-02-26 20:07:53 +0100

    fixed typo

--- c++/socket/examples/compile_mingw.bat
+++ c++/socket/examples/compile_mingw.bat
@@ -4,4 +4,4 @@ g++ -I.. ..\socket.cpp EchoServer.cpp  -lWs2_32 -oEchoServer.exe
 
 g++ -I.. -I..\..\misc ..\socket.cpp Proxy.cpp ..\..\misc\Chameleon.cpp -lWs2_32 -oProxy.exe
 
-g++ -I.. ..\socket.cpp MessageDistrubutor.cpp -lWs2_32 -oMessageDistributor.exe
+g++ -I.. ..\socket.cpp MessageDistributor.cpp -lWs2_32 -oMessageDistributor.exe
