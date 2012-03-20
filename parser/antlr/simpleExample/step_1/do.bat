086bdb67915afe4519d0fe5de63b72f482aa14b6
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-03-20 16:45:35 +0100

    Explicit use of -cp

--- parser/antlr/simpleExample/step_1/do.bat
+++ parser/antlr/simpleExample/step_1/do.bat
@@ -2,8 +2,6 @@
 @rem Be sure to have %CLASSPATH% set so that
 @rem it points to antlr-3.3-complete.jar
 
-@set CLASSPATH=.;c:\Users\tq84\Downloads\antlr-3.3-complete.jar
-
 @rem --------------------------
 @rem The following line creates:
 @rem 
@@ -12,12 +10,12 @@
 @rem     o ExpLexer.java
 @rem
 
-     java org.antlr.Tool Exp.g
+     java -cp .;%CLASSPATH% org.antlr.Tool Exp.g
 
 @rem --------------------------
 @rem Use the Test Class
 
-     javac ANTLRDemo.java 
+     javac -cp .;%CLASSPATH% ANTLRDemo.java 
 
-     java -cp %CLASSPATH% ANTLRDemo
+     java  -cp .;%CLASSPATH% ANTLRDemo
 
