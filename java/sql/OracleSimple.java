52d13c441d2c5e06917734c4958913f19359ad6e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-04-03 19:25:05 +0200

    + java/sql

---
+++ java/sql/OracleSimple.java
@@ -0,0 +1,47 @@
+//
+//        javac OracleSimple.java
+//
+//        set JDBCJAR=%ORACLE_HOME%\jdbc\lib\ojdbc5.jar
+//        set ORAUSER=spezmdba
+//        set ORAPW=***********
+//        set DBSERVER=dbsrv70a.eng.zkb.ch
+//        set DBPORT=10922
+//        set DBNAME=AMDS64.eng.zkb.ch
+//
+//        java -cp ".;%JDBCJAR%"  OracleSimple  rene  rene  localhost 1521 ORA11R2
+//
+//
+// ------------------------------------
+import java.sql.*;
+class OracleSimple {
+
+  public static void main (String[] args) throws Exception {
+
+     if (args.length < 4) {
+         System.out.println("Expected parrameters:");
+         System.out.println("  username  password  hostname  dbport  dbname");
+         return;
+     }
+
+     Class.forName ("oracle.jdbc.OracleDriver");
+
+     String username = args[0];
+     String password = args[1];
+     String hostname = args[2];
+     String dbport   = args[3];
+     String dbname   = args[4];
+  
+     Connection conn = DriverManager.getConnection( "jdbc:oracle:thin:@//" + hostname + ":" + dbport + "/" + dbname, 
+                                                    username,
+                                                    password );
+  
+     Statement stmt = conn.createStatement();
+  
+//   stmt.setFetchSize(1);
+  
+     ResultSet rset = stmt.executeQuery("select table_name from user_tables");
+  
+     while (rset.next())
+       System.out.println (rset.getString(1)); 
+    }
+}
