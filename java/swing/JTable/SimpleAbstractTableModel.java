cd0736dc90d54316bcb4e949f4421f76df209f69
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-04-12 22:20:06 +0200

    SimpleAbstractTableModel.java

---
+++ java/swing/JTable/SimpleAbstractTableModel.java
@@ -0,0 +1,48 @@
+//     http://www.kodejava.org/examples/699.html
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+
+public class SimpleAbstractTableModel extends JFrame {
+
+
+       class  AbstractTableModel_ extends AbstractTableModel {
+
+              public int getRowCount   () { return 4; }
+              public int getColumnCount() { return 3; }
+
+              public Object getValueAt(int rowIndex, int columnIndex) {
+                     return (rowIndex+1) * (columnIndex+2);
+              }
+
+              public String getColumnName(int column) {
+                     return "Col " + column;
+              }
+
+              public Class<?> getColumnClass(int columnIndex) {
+                     return int.class.getClass();
+              }
+       }
+
+
+
+       SimpleAbstractTableModel() {
+
+              super("Simple AbstractTableModel example");
+
+              setSize (500, 500);
+
+              JTable table = new JTable(new AbstractTableModel_());
+              table.setFillsViewportHeight(true);
+              add(table);
+
+              setDefaultCloseOperation(EXIT_ON_CLOSE);
+              setVisible(true);
+
+       }
+
+       public static void main(String[]_) {
+              new SimpleAbstractTableModel();
+       }
+
+}
