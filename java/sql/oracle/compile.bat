dfd83caa1787cc85ae9035c7da6bb699e6d6f154
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-04-14 08:23:41 +0200

    n/a

--- java/sql/oracle/compile.bat
+++ java/sql/oracle/compile.bat
@@ -2,4 +2,5 @@ javac ConnectionCreator.java
 javac CreateTable.java
 javac InsertTable.java
 javac SelectTable.java
+javac MetaData.java
 javac DropTable.java
--- java/sql/oracle/run.bat
+++ java/sql/oracle/run.bat
@@ -3,4 +3,5 @@ set JDBCJAR=%ORACLE_HOME%\jdbc\lib\ojdbc5.jar
 java -cp ".;%JDBCJAR%" CreateTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
 java -cp ".;%JDBCJAR%" InsertTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
 java -cp ".;%JDBCJAR%" SelectTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
+java -cp ".;%JDBCJAR%"    MetaData  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
 java -cp ".;%JDBCJAR%"   DropTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
