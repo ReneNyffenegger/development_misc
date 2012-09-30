322ab9f81df10e2bb5a4541ab2c261ba9ea22ec5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-09-30 21:57:24 +0200

    close (con)

--- R/RODBC/connect.R
+++ R/RODBC/connect.R
@@ -9,3 +9,5 @@ con <- odbcConnect("ORA11R2", uid="rene", pwd="rene")
 tables = sqlQuery(con, "select owner, table_name from dba_tables where owner not in ('SYS', 'SYSTEM')")
 
 print(tables)
+
+close(con)
