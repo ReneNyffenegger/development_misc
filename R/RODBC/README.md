4a632e08bc7bdb9f1df3e4b64f7c991b037596cd
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-09-30 21:55:44 +0200

    + R/RODBC

---
+++ R/RODBC/README.md
@@ -0,0 +1,4 @@
+
+
+    install.packages("c:/temp/RODBC_1.3-6.zip", repos=NULL)
+
---
+++ R/RODBC/connect.R
@@ -0,0 +1,11 @@
+#
+#
+#
+
+library(RODBC)
+
+con <- odbcConnect("ORA11R2", uid="rene", pwd="rene")
+
+tables = sqlQuery(con, "select owner, table_name from dba_tables where owner not in ('SYS', 'SYSTEM')")
+
+print(tables)
