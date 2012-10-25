18ff2de6666d91036bd98bd2f11cb6dc1ee0ebfe
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-10-25 06:40:26 +0200

    Difference in seconds

--- perl/cpan/Date/Manip.pl
+++ perl/cpan/Date/Manip.pl
@@ -8,6 +8,18 @@ parse_date('today'     );  # 2012102500:00:00 (on Oct 25th 2012).
 parse_date('yesterday' );  # 2012102400:00:00 (on Oct 25th 2012).
 parse_date('08/28/1970');  # 1970082800:00:00
 parse_date('8am'       );  # 2012102508:00:00
+parse_date('8:13am'    );  # 2012102508:00:00
+parse_date('8:13:22am' );  # 2012102508:00:00
+parse_date('now'       );  # 2012102508:00:00
+
+#
+
+my $now   = ParseDate('now');
+my $later = ParseDate('8:13am');
+
+#  %s extracts seconds since Jan 1st 1970. So it's possible to get the difference 
+#  of two dates in seconds:
+print "Difference between $now and $later: " . (UnixDate($later, "%s") - UnixDate($now, "%s")) . " seconds\n"; 
 
 sub parse_date {
   my $txt = shift;
