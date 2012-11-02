a6550a751608d45713e23f7ffb4fd82a254ca32c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-11-02 17:38:30 +0100

    + packages

---
+++ perl/packages/Ex1.pl
@@ -0,0 +1,4 @@
+use warnings;
+use strict;
+
+use Ex1;
---
+++ perl/packages/Ex1.pm
@@ -0,0 +1,10 @@
+use warnings;
+use strict;
+
+use feature 'say';
+
+package Ex1;
+
+say "This line is printed when the package is 'used'";
+
+1;
