b4762c96be3c53b231db2abc884546c2f100412a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-11-23 22:19:15 +0100

    + DBD::Oracle

---
+++ perl/cpan/DBD/Oracle/ref_cursor/04_perl.pl
@@ -0,0 +1,32 @@
+use DBI;
+use DBD::Oracle qw(:ora_types);
+
+use warnings;
+use strict;
+
+print "User: ";     my $user = <STDIN>;
+print "Password: "; my $pw   = <STDIN>;
+print "SID: ";      my $sid  = <STDIN>;
+
+chomp($user, $pw, $sid);
+
+my $dbh = DBI->connect("dbi:Oracle:$sid", $user, $pw);
+
+my $sth = $dbh -> prepare("
+begin
+  open :rc for 
+    select * from perl_refcursor_tab
+     where a > :p_a;
+end;
+");
+
+$sth->bind_param(":p_a", 5);
+
+my $sth_cursor;
+$sth->bind_param_inout(":rc", \$sth_cursor, 0, {ora_type => ORA_RSET });
+
+$sth->execute();
+
+while (my $r = $sth_cursor->fetchrow_hashref) {
+  print $r->{A}, "\n";
+}
---
+++ perl/cpan/DBD/Oracle/ref_cursor/all.sql
@@ -0,0 +1,5 @@
+start  01_table.sql
+start  02_function.sql
+
+host   03_perl.pl
+host   04_perl.pl
