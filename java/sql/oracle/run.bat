953fb3d8191dc2b74e138f50d7e628ba755a42b5
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-10-09 17:08:20 +0200

    add some values to variables

--- java/sql/oracle/run.bat
+++ java/sql/oracle/run.bat
@@ -1,4 +1,10 @@
-set JDBCJAR=%ORACLE_HOME%\jdbc\lib\ojdbc5.jar
+set JDBCJAR=%ORACLE_HOME%\jdbc\lib\ojdbc8.jar
+
+set ORAUSER=rene
+set ORAPW=rene
+set DBSERVER=localhost
+set DBPORT=1521
+set DBNAME=ora18
 
 java -cp ".;%JDBCJAR%" CreateTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
 java -cp ".;%JDBCJAR%" InsertTable  %ORAUSER%  %ORAPW%  %DBSERVER%  %DBPORT%  %DBNAME%
